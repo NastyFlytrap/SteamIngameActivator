966e12c9d28d7df13c5befb10172bed72160d71f
BOSS <BOSS@BOSS>
2021-06-28 13:35:12 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SteamIngameActivator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+          InitializeComponent();
+        }
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            OpenFileDialog opentxt;
+            OpenFileDialog openFileDialog = new();
+            OpenFileDialog openFileDlg = openFileDialog;
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+                FileNameTextBox.Text = openFileDlg.FileName;
+                TextBlock1.Text = System.IO.File.ReadAllText(openFileDlg.FileName);
+            }
+        }
+    }
+}
